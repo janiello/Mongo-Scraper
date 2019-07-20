e9be92d91caa7ad527a4713007d6aaf220e68812
janiello <janiello417@gmail.com>
2019-07-20 17:48:51 -0400

    created initial files and folder structure. Coded boilerplate server.js to get the app running on node. Began coding basic scraper tools.

---
+++ server.js
@@ -0,0 +1,37 @@
+var express = require("express");
+var mongoose = require("mongoose");
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+var db = require("./models");
+
+var PORT = 3000;
+
+var app = express();
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static("public"));
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI, {useNewUrlParser: true});
+
+/*app.get("/scrape", function(req, res) {
+    axios.get("https://www.everydayshouldbesaturday.com/").then(function(response) {
+        var $ = cheerio.load(response.data);
+        $("div.c-entry-box--compact--article").each(function(i, element) {
+            var result = {};
+            result.title = $(this)
+                .children("a")
+                .text();
+            result.link = $(this)
+                .children("a")
+                .text("href");
+        })
+    })
+})*/
+
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT + "!");
+});
