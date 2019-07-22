8cb9aa7a411a80075bc9ccc039979b1bd4f9d231
janiello <janiello417@gmail.com>
2019-07-21 20:06:12 -0400

    alterd app.js to add a comment button for every article that gets pulled from the database

--- public/app.js
+++ public/app.js
@@ -1,5 +1,16 @@
 $.getJSON("/articles", function(data) {
     for (var i = 0; i < data.length; i++) {
-        $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
-    }
+        $("#articles").append (
+            "<p data-id='" + data[i]._id + "'>" +
+            data[i].title +
+            "<button class='comment'>" +
+            "Comment" +
+            "</button>" +
+            "<br />" +
+            "<a href=" + data[i].link + ">" +
+            data[i].link +
+            "</a>" +
+            "</p>"
+        );
+    };
 });
