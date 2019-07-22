0b4c07bf987eaecb5040d86b95b2742b56a6d633
janiello <janiello417@gmail.com>
2019-07-21 22:06:49 -0400

    added comment functions in app.js so users could comment on articles. Didn't work, but I need to move on to the next homework. Might come back to finish this assignment later

--- public/app.js
+++ public/app.js
@@ -3,9 +3,7 @@ $.getJSON("/articles", function(data) {
         $("#articles").append (
             "<p data-id='" + data[i]._id + "'>" +
             data[i].title +
-            "<button class='comment'>" +
-            "Comment" +
-            "</button>" +
+            "<button id='comment'>Comment</button>" +
             "<br />" +
             "<a href=" + data[i].link + ">" +
             data[i].link +
@@ -13,4 +11,42 @@ $.getJSON("/articles", function(data) {
             "</p>"
         );
     };
+});
+
+$(document).on("click", "#comment", function() {
+    $("#notes").empty();
+    var thisId = $("p").attr("data-id");
+    $.ajax({
+        method: "GET",
+        url: "/articles/" + thisId
+    })
+    .then(function(data) {
+        console.log(data);
+        $("#notes").append("<h2>" + data.title + "</h2>");
+        $("#notes").append("<input id='titleinput' name='title'>");
+        $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
+        $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
+        if (data.note) {
+            $("#titleinput").val(data.note.title);
+            $("#bodyinput").val(data.note.body);
+        }
+    });
+});
+
+$(document).on("click", "#savenote", function() {
+    var thisId = $(this).attr("data-id");
+    $.ajax({
+        method: "POST",
+        url: "/articles/" + thisId,
+        data: {
+            title: $("#titleinput").val(),
+            body: $("#bodyinput").val()
+        }
+    })
+    .then(function(data) {
+        console.log(data);
+        $("#notes").empty();
+    });
+    $("#titleinput").val("");
+    $("bodyinput").val("");
 });
